f4370f91fa3d1befa6833d9582e66c9fc22803c9
Artem Galay <iostema@mail.ru>
2022-11-16 19:17:31 +0300

    Initial Commit

---
+++ CoreDataLesson/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataLesson
+//
+//  Created by Артем Галай on 16.11.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
